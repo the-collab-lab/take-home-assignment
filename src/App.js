994c99760b7220843a6b92f3acd8fcefb3740cde
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2021-02-23 17:12:52 +0000

    Add basic form submitting state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import React from "react";
 
 function App() {
+  const [textInput, setTextInput] = React.useState("")
+
+  const handleChange = (event) => {
+    setTextInput(event.target.value);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTextInput("")
+  }
+  
   return (
     <div className="App">
       <header>
         <h1>Career Lab | Take-Home Assignment</h1>
       </header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          <textarea value="Enter text here"/>
+          <textarea onChange={handleChange} value={textInput}/>
         </label>
         <input type="submit" value="Submit"/>
       </form>
