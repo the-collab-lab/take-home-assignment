539dc8c7c2b42f8720d7b7520e2b5f71dbd276e2
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2021-02-24 17:16:44 +0000

    Add a div for the formatted text

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React from "react";
 
 function App() {
   const [textInput, setTextInput] = React.useState("")
-
+  const [formattedText, setFormattedText] = React.useState(null)
   const handleChange = (event) => {
     setTextInput(event.target.value);
   }
   const handleSubmit = (event) => {
     event.preventDefault();
+    setFormattedText(textInput)
     setTextInput("")
   }
-  
+
   return (
     <div className="App">
       <header>
@@ -23,6 +24,7 @@ function App() {
         </label>
         <input type="submit" value="Submit"/>
       </form>
+      {formattedText && <div>{formattedText}</div>}
     </div>
   );
 }
