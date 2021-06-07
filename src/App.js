19f23af1350dd1ac7274abff4848461677610056
jamesncox <jamesncox@yahoo.com>
2021-06-07 16:55:38 -0500

    Add comments detailing the functions I'll need for each button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App() {
     setTextOutput("Your formatted text will go here!");
   };
 
+  // Create two functions, handleUpperCase and handleLowerCase that are
+  // wired up to the two buttons for toggling textOutput size
+
+  // If I create a button for clearing textInput and textOutput,
+  // create a function called clearTextInputAndOutput that resets the state of each.
+
   return (
     <div className="App">
       <header>
@@ -30,7 +36,7 @@ function App() {
       <div id="result">{textOutput}</div>
       {/* Create a UI to select/switch between these two modes. */}
       {/* Create two buttons, one that handles upper-casing textOutput and the other that handles lower-casing textOutput */}
-      {/* Could be nice to have a button that also that clears textInput */}
+      {/* Could be nice to have a button that also that clears textInput and textOutput*/}
     </div>
   );
 }
